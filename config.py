39002b16f0369982bd1171d94794f9ddbe677577
Ivar Grytten <ivar@1337.no>
2016-06-22 11:16:19 +0200

    developing

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DATA_PATH = "data/tmp/"
 
 DEBUG = False
 
-if os.path.dirname(os.path.realpath(__file__)) == "/home/ivarandknut/checkout_genomic_intervals/reproduce":
+if os.path.dirname(os.path.realpath(__file__)) == "/home/ivarandknut/checkout_genomic_intervals":
     DATA_PATH = "/var/www/web/data/tmp/"
     #DATA_PATH = "../data/tmp/"
 
