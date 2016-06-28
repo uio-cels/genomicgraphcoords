98a9ae34f6938c1c697ca67b614d135779e659fd
Ivar Grytten <ivar@1337.no>
2016-06-28 09:27:33 +0200

    developing

--- Visualize.py
+++ Visualize.py
@@ -122,8 +122,8 @@ class VisualizeHtml():
         self.graph = graph
         self.color_counter = 4
         self.colors = list(six.iteritems(colors.cnames))[6:]
-        self.colors = ["#0000aa", "lightblue", "darkred", "#cccccc", "purple", "orange", "indigo", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black"]
-        self.gene_colors = ["darkorange", "#0000cc", "#aaaaaa", "pink"]
+        self.colors = ["#0000aa", "lightblue", "darkred", "#cccccc", "purple", "orange", "indigo"]
+        self.gene_colors = ["darkorange", "#0000cc", "#aaaaaa", "pink", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black"]
         self.gene_counter = 0
         self.genes_plotted_heights = {} # Dict of heights for genes
         self.offset_positions = {}  # Dict of offset pos for each alt loci/chrom
