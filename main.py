185e78ba2b5ed67f45441ae09ec8615f3ccd1e86
Knut Rand <knutdrand@gmail.com>
2016-09-13 13:52:23 +0200

    call func

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def get_flanking_alignments(region_name, alt_info):
                              alt_info["chromStart"] + start_flank_length)
 
     linRef2 = LinearInterval("hg38", alt_info["chrom"], 0, start_flank_length)
-    start_pair = (lineRef1, linRef2)
+    start_pair = (linRef1, linRef2)
 
     stop_flank_length = 0
     for i in xrange(min(len(consensus_seq), len(alt_seq))):
@@ -88,6 +88,7 @@ def get_flanking_alignments(region_name, alt_info):
 
 segments = []
 
+
 def create_align_graph(region_name, min_length):
     dbw = DbWrapper()
     chromosome_ids = ["chr%s" % n for n in range(1, 23)+["X", "Y"]]
@@ -104,9 +105,12 @@ def create_align_graph(region_name, min_length):
 
     alt_info = dbw.alt_loci_info(region_name)
 
-    align_file = get_alignments(region_name, alt_info)
-    alignments = get_filtered_alignments(align_file, alt_info["chromStart"])
 
+
+
+    #align_file = get_alignments(region_name, alt_info)
+    #alignments = get_filtered_alignments(align_file, alt_info["chromStart"])
+    alignments = get_flanking_alignments(region_name, alt_info)
     #print "Alignments :"
     #print alignments
     if len(alignments) == 0:
