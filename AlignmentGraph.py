9530915d02a94579d7c51e3cb5641d1635a406f5
Knut Rand <knutdrand@gmail.com>
2016-09-20 16:11:22 +0200

    update coordinates in alignment graph

--- AlignmentGraph.py
+++ AlignmentGraph.py
@@ -15,8 +15,8 @@ def parse_path(parts, l):
 
 
 def parse_edge(parts):
-    out_node = parts[1]
-    in_node = parts[3]
+    out_node = int(parts[1])
+    in_node = int(parts[3])
     return (out_node, in_node)
 
 
@@ -28,7 +28,9 @@ def parse_gfa(filename):
     cur_intervals = []
     for line in f.readlines():
         parts = line.split()
-        _id = parts[1]
+        if parts[0] == "H":
+            continue
+        _id = int(parts[1])
         if cur_id != _id:
             if cur_len is not None:
                 graph.blocks[cur_id] = RegionPath(
@@ -45,10 +47,43 @@ def parse_gfa(filename):
             if out_id not in graph.block_edges:
                 graph.block_edges[out_id] = []
             graph.block_edges[out_id].append(in_id)
+    graph.blocks[cur_id] = RegionPath(
+        cur_id, {li.chromosome: li for li in cur_intervals})
 
     return graph
 
+
+def update_offsets(graph):
+    start_blocks_ids = [block_id for block_id in graph.blocks if
+                        not any([block_id in in_edges for
+                                 in_edges in graph.block_edges.values()])]
+    for start_block_id in start_blocks_ids:
+        print(start_block_id)
+        chromosomes = graph.blocks[start_block_id].linear_references.keys()
+        for chromosome in chromosomes:
+            print(chromosome)
+            cur_block = graph.blocks[start_block_id]
+            offset = 0
+            while True:
+                offset += cur_block.get_length()
+                if cur_block.id not in graph.block_edges:
+                    break
+                next_blocks = [
+                    e for e in graph.block_edges[cur_block.id] if
+                    chromosome in graph.blocks[e].linear_references]
+                if not next_blocks:
+                    break
+                cur_block = graph.blocks[next_blocks[0]]
+                lin_ref = cur_block.linear_references[chromosome]
+                lin_ref.start += offset
+                lin_ref.end += offset
+
+
 if __name__ == "__main__":
     graph = parse_gfa("tmp.gfa")
-    print graph.blocks.keys()
-    print graph.block_edges
+    for v in graph.blocks.values():
+        print(v)
+    print(graph.block_edges)
+    update_offsets(graph)
+    for v in graph.blocks.values():
+        print(v)
