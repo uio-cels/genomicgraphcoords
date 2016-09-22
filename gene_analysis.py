9c0043c89e8e5835e59d4d66a9e9a1f0cc7e3fb7
Knut Rand <knutdrand@gmail.com>
2016-09-22 14:22:52 +0200

    Working code based gene analysis

--- gene_analysis.py
+++ gene_analysis.py
@@ -41,21 +41,75 @@ def get_flanking_lins():
 
 def get_overlap_code(gene, loci_partition):
     full_loci = loci_partition[0] + loci_partition[1] + loci_partition[2]
-    codes = (
+    return (
         not full_loci.contains(gene),
-        gene.intersects(loci_partition[0]) or gene.intersects(loci_partition[2]),
+        gene.intersects(loci_partition[0]) or
+        gene.intersects(loci_partition[2]),
         gene.intersects(loci_partition[1]),
-        gene.intersects(loci_partition[0]) and gene.intersects(loci_partition[2]),
+        gene.intersects(loci_partition[0]) and
+        gene.intersects(loci_partition[2]),
         gene.contains(full_loci))
 
 
-def find_main_genes_by_codes(gene_intervals, lin_ref_dict, code):
+def code_more(a, b):
+    for v1, v2 in zip(a, b):
+        if (not v1) and v2:
+            return False
+    return True
+
+
+def code_less(a, b):
+    for v1, v2 in zip(a, b):
+        if v1 and not v2:
+            return False
+    return True
+
+
+def code_equal(a, b):
+    return a == b
+
+
+def find_main_genes_by_codes(gene_intervals, lin_ref_dict, code,
+                             cmpr=code_equal):
     """
     Find genes that overlap with an alt_loci in the way specified by code
     """
     return [gi for gi in gene_intervals if
-            any([get_overlap_code(gi, partition) == code
+            any([cmpr(get_overlap_code(gi, partition), code)
                  for partition in lin_ref_dict[gi.chromosome]])]
+
+
+def get_alt_gene_stats():
+    db = DbWrapper()
+    genes = db.get_alt_genes()
+    gene_intervals = [interval_from_gene(gene) for gene in genes]
+    alt_loci_infos = db.get_alt_loci_infos(False)
+    partitions = [get_flanks(ali) for ali in alt_loci_infos]
+    alt_partitions = [par[1] for par in partitions]
+
+    lin_ref_dict = {}
+    for gene in gene_intervals:
+        lin_ref_dict[gene.chromosome] = []
+
+    for i, ali in enumerate(alt_loci_infos):
+        chrom = ali["name"]
+        if chrom not in lin_ref_dict:
+            lin_ref_dict[chrom] = []
+        lin_ref_dict[chrom].append(alt_partitions[i])
+
+    pure_alt_code = (False, False, True, False, False)
+    pure_flank_code = (False, True, False, False, False)
+    alt_genes = find_main_genes_by_codes(
+        gene_intervals, lin_ref_dict, pure_alt_code)
+    flank_genes = find_main_genes_by_codes(
+        gene_intervals, lin_ref_dict, pure_flank_code)
+    spanning_genes = find_main_genes_by_codes(
+        gene_intervals, lin_ref_dict, pure_alt_code, code_more)
+
+    print("All genes", len(gene_intervals))
+    print("Alt genes", len(alt_genes))
+    print("Flank genes", len(flank_genes))
+    print("Spanning genes", len(spanning_genes)-len(alt_genes))
 
 
 def get_main_gene_stats():
@@ -77,11 +131,24 @@ def get_main_gene_stats():
         lin_ref_dict[chrom].append(main_partitions[i])
 
     old_spanning_codes = (True, True, False, False, False)
+
     spanning_genes = find_main_genes_by_codes(
         gene_intervals, lin_ref_dict, old_spanning_codes)
-    for gene in spanning_genes:
-        print(gene)
-    print(len(spanning_genes))
+
+    alt_code = (False, False, True, False, False)
+    alt_genes = find_main_genes_by_codes(gene_intervals, lin_ref_dict,
+                                         alt_code)
+
+    new_spanning_genes = find_main_genes_by_codes(
+        gene_intervals, lin_ref_dict, alt_code, code_more)
+
+    pure_flankning_genes = find_main_genes_by_codes(
+        gene_intervals, lin_ref_dict, (False, True, False, False, False))
+    print("All genes", len(gene_intervals))
+    print("Pure alt_genes", len(alt_genes))
+    print("Spanning alt genes", len(new_spanning_genes)-len(alt_genes))
+    print("Flanking genes", len(pure_flankning_genes))
+    print("Flank spanning genes",  len(spanning_genes))
 
 
 def calculate_original_main_spans():
@@ -113,7 +180,7 @@ def calculate_original_main_spans():
                                interval in lin_ref_dict[gi.chromosome]])
                           ]
     crossing_genes = [gi for gi in intersecting_genes if
-                      any([(interval[0].intersects(gi) and not interval[0].contains(gi)) and
+                      any([(interval[0].intersects(gi) and not interval[0].contains(gi)) or
                            (interval[2].intersects(gi) and not interval[2].contains(gi)) for
                            interval in lin_ref_dict[gi.chromosome]])]
 
@@ -167,9 +234,9 @@ def calculate_main_spans():
 
 
 if __name__ == "__main__":
-    #  calculate_original_main_spans()
-    print("------------------------------------------")
-    get_main_gene_stats()
+    get_alt_gene_stats()
+    # get_main_gene_stats()
+
     # get_flanking_lins()
     # calculate_main_spans()
     exit(0)
