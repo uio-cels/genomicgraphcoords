32c3c27369d11687e2134841e132e0bb011a5eb8
Ivar Grytten <ivar@1337.no>
2016-10-18 14:40:44 +0200

    web server dev

--- interface.py
+++ interface.py
@@ -121,7 +121,7 @@ elif method == "align_region2" or method == "align_region_html":
     print("<hr>")
     print(graph.block_edges)
     print("<hr>")
-    
+
     v = VisualizeHtml(graph, area_start, area_end, 0, description, 800, gene_segments)
 
     print("<p>GENE SEGMENTS</p>")
