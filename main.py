dd9a8cc7b457c783552783b5be664626ca1d02b5
Ivar Grytten <ivar@1337.no>
2016-10-19 11:47:58 +0200

    webtool dev

--- main.py
+++ main.py
@@ -256,10 +256,19 @@ def create_align_graph(region_name, min_length):
 
         # Create one interval for each exon
         ei = 0
-        ex_ends = gene["exonEnds"].decode('utf8').split(",")
+
+        # Hack, because exons are sometimes str, sometimes byte
+        if isinstance(str, gene["exonEnds"]):
+            ex_ends = gene["exonEnds"].split(",")
+            ex_starts = gene["exonStarts"].split(",")
+        else:
+            ex_ends = gene["exonEnds"].decode('utf8').split(",")
+            ex_starts = gene["exonStarts"].split(",")
+
         #ex_ends = gene["exonEnds"].split(",")
-        for ex in gene["exonStarts"].decode('utf8').split(","):
+        #for ex in gene["exonStarts"].decode('utf8').split(","):
         #for ex in gene["exonStarts"].split(","):
+        for ex in ex_starts:
             if ex == "":
                 continue
 
