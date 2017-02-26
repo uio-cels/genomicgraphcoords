2b5da5a910399fa6485751fa269c16f82044347d
ivar <ivar@ivar>
2017-02-26 10:44:20 +0100

    web-tool dev

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 
 import os
 
-DATA_PATH = "data/tmp/"
-
 DEBUG = False
 
-if os.path.dirname(os.path.realpath(__file__)) == "/home/ivarandknut/checkout_genomic_intervals":
-    DATA_PATH = "/var/www/web/data/tmp/"
-    #DATA_PATH = "../data/tmp/"
 
