a3b3eb8e13d2ff62de4c86a1ba2c4a4cb3626444
ivar <ivar@ivar>
2017-02-26 13:23:55 +0100

    web-tool dev

--- visualizehtml.py
+++ visualizehtml.py
@@ -265,7 +265,7 @@ class VisualizeHtml(object):
         if not is_exon:
             self.html_intervals[block][self.gene_counter] = html
         else:
-            self.html_intervals[block][self.gene_counter] += html + "</div>"
+            self.html_exons[block][self.gene_counter] += html + "</div>"
 
 
     def _coordinate(self, rp):
