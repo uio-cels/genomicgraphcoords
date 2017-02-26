0dadbbab8a54befd82921ccf50b8dd5afa5c32d1
ivar <ivar@ivar>
2017-02-26 15:20:24 +0100

    web-tool dev

--- visualizehtml.py
+++ visualizehtml.py
@@ -51,9 +51,10 @@ class VisualizeHtml(object):
                 <h4>%s</h4>
             </div>
             <div class='col-md-4'>
-				<div class='options'>
+				<!--<div class='options'>
 					<p><label><input type='checkbox' onclick="$('.exon').toggle();"> Click to show exons <span id='exon_cnt'></span></label></p>
 				</div>
+			    -->
 			</div>
         """ % description
 
